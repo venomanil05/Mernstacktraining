0afdd7b3b18079ce598b4173df12862c0a774b5a
Anil Pal <anilpalthakuri@gmail.com>
2026-08-25 12:59:31 +0545

    update

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,17 +1,32 @@
-import product from "../model/product.js";
+import Product from "../model/product.js";
 import user from "../model/user.js";
 
 const getproducts= async (req, res) => {
-    const products = await product.find();
+    const products = await Product.find();
     res.send(products);
 };
 
-const addproduct= (req,res)=>{
-    
+const addproduct= async (req,res) => {
+    const newproduct ={
+        name:'Sample Name',
+        price:70,
+        description:'sample description',
+        brand:'sample brand',
+        category:'sample category',
+    }; 
+    const product = await Product.create(newproduct);
+    res.send({message:"product added successfully!"});
 };
 
-const getproductbyid= (req,res)=>{
-    
+// /api/products/:id
+const getproductbyid= async (req,res)=>{
+    const {id} = req.params;
+    const product = await Product.findById(id)
+    if(product){
+        res.send(product);
+    } else{
+        res.status(404).send({error:"product not found!"});
+    }
 };
 
 export{ getproducts,getproductbyid, addproduct };

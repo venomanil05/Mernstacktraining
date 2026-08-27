0971d1d73bc427ac1116955a5521157d3a4689fd
Anil Pal <anilpalthakuri@gmail.com>
2026-08-27 10:57:23 +0545

    update

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,37 +1,51 @@
 import User from '../model/user.js';
+import createToken from '../utils/createToken.js';
 
 const signup = async (req, res) => {
     const { fullname, email, password, isAdmin } = req.body;
+
     const user = await User.findOne({ email });
     if (user) return res.status(400).send({ error: "User already exists" });
+
     const newUser = await User.create({ fullname, email, password, isAdmin });
 
-    res.send({ message: "User created successfully", 
+    res.send({
+        message: "User created successfully",
         user: {
-        fullname: newUser.fullname,
-        email: newUser.email,
-        isAdmin: newUser.isAdmin,
-    },
- });
+            fullname: newUser.fullname,
+            email: newUser.email,
+            isAdmin: newUser.isAdmin,
+        },
+    });
 };
 
-const login = async (req,res) =>{
-    const {email,password} = req.body;
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
     const user = await User.findOne({ email });
-    if(!user) return res.status(404).send({error:"User not registered!"});
-    if(await user.comparePassword(password)){
+    if (!user) {
+        return res.status(404).send({ error: "User not registered!" });
+    }
+
+    if (await user.comparePassword(password)) {
+        createToken(user._id, res);
+
         res.send({
-            message:"login success!",
-            user:{
-                fullname:user.fullname,
-                email:user.email,
-                isAdmin:user.isAdmin,
+            message: "login success!",
+            user: {
+                fullname: user.fullname,
+                email: user.email,
+                isAdmin: user.isAdmin,
             },
         });
     } else {
-        res.status(400).send({error:"password not matched"});
+        res.status(400).send({ error: "password not matched" });
     }
 };
 
+const logout = async (req, res) => {
+    res.clearCookie("jwt"); 
+    res.send({mesage:"Logout Success!"});
+    };
 
-export { signup , login };
+export { signup, login, logout };
--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const createToken = (_id, res) => {
    const token = jwt.sign({_id}, 'mysecretkey', {
-    expiresIn:'1m'
+    expiresIn:'3d'
    });
    res.cookie('jwt',token,{
     maxAge: 3 * 24 * 60 *60 * 1000,
